f101a4f244da828b8a485d31b572506a2e4dc82c
Vishnu pv <71384832+pvvishnu557@users.noreply.github.com>
2021-07-15 15:25:13 +0530

    Add files via upload

---
+++ insta logo.py
@@ -0,0 +1,24 @@
+from turtle import*
+
+bgcolor('black')
+pencolor('yellow')
+width(24)
+penup()
+goto(160,-100)
+pendown()
+
+
+left(90)
+for i in range(4):
+    forward(250)
+    circle(34,90)
+
+
+penup()
+goto(80,30)
+pendown()
+circle(80,360)
+penup()
+goto(110,130)
+pendown()
+circle(7,360)
